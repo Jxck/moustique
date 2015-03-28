c3c60f0875e1bec7519a6e430051bfc51108b202
Jxck <block.rxckin.beats@gmail.com>
2015-03-28 14:47:34 +0900

    add initial client

---
+++ index.js
@@ -0,0 +1,33 @@
+var mqtt = require('mqtt');
+
+function Client(username) {
+  this.username = username;
+  this.connection = null;
+}
+
+Client.prototype.connect = function(url) {
+  if(url === undefined) {
+    throw new Error('url required');
+  }
+  this.connection = mqtt.connect(url, { clientId: this.username });
+
+  this.connection.on('connect', function() {
+    console.log('connect');
+  });
+
+  this.connection.on('message', function(topic, payload) {
+    var message = JSON.parse(payload.toString());
+    console.log('message', topic, message);
+  });
+}
+
+Client.prototype.sub = function(topic, callback) {
+  console.log('add handler to', topic);
+  this.connection.subscribe(topic);
+}
+
+Client.prototype.pub = function(topic, data) {
+  console.log('publish', topic, data);
+  var message = JSON.stringify(data);
+  this.connection.publish(topic, message);
+}
