59e97ff60ed622dab8485eb622f90a36291f80c0
Jxck <block.rxckin.beats@gmail.com>
2015-03-28 15:05:18 +0900

    add appname to client

--- client.js
+++ client.js
@@ -2,8 +2,11 @@ var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var mqtt = require('mqtt');
 
-function Client(username) {
+var Router = require('./router');
+
+function Client(appname, username) {
   EventEmitter.call(this);
+  this.appname = appname;
   this.username = username;
   this.connection = null;
 }
@@ -27,11 +30,13 @@ Client.prototype.connect = function(url) {
 }
 
 Client.prototype.sub = function(topic, callback) {
+  var topic = util.format('/%s/%s', this.appname, this.topic);
   console.log('add handler to', topic);
   this.connection.subscribe(topic);
 }
 
 Client.prototype.pub = function(topic, data) {
+  var topic = util.format('/%s/%s', this.appname, this.topic);
   console.log('publish', topic, data);
   var message = JSON.stringify(data);
   this.connection.publish(topic, message);
