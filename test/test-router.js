e32815cdb06eace86195ce08ae3c2d4f570c5c20
Jxck <block.rxckin.beats@gmail.com>
2015-04-08 15:28:54 +0900

    update test

--- test/test-router.js
+++ test/test-router.js
@@ -5,16 +5,15 @@ function assert(actual, expected) {
   console.assert(actual === expected, '\nact: ' + actual + '\nexp: ' + expected);
 }
 
-function deepEqual(x, y) {
-  let result = Object.keys(x).map((key) => {
-    return x[key] === y[key];
-  }).reduce((curr, pre) => {
-    return curr && pre
-  });
-  console.assert(result);
+function deepEqual(actual, expected) {
+  if (require) {
+    require('assert').deepEqual(actual, expected);
+  } else {
+    assert(JSON.stringify(actual), JSON.stringify(expected));
+  }
 }
 
-function test1() {
+function sample() {
   let router = new Router();
   router.topic('/id/:id/name/:name', (message) => {
     assert(message.params.id, '1');
@@ -26,32 +25,177 @@ function test1() {
   router.route('/id/1/name/foo', 'testdata');
 };
 
-function test2() {
-  let router = new Router();
-  [
+function match() {
+  let testcase = [
     {
-      pub: '/id/1/name/foo',
       sub: '/id/:id/name/:name',
+      pub: '/id/1/name/foo',
       params: {
         id: '1',
         name: 'foo'
       },
       message: 'test'
-    }
-  ].forEach((test) => {
+    },
+    {
+      sub: '/id/1/name/foo',
+      pub: '/id/1/name/foo',
+      params: {},
+      message: 'test'
+    },
+    {
+      sub: '/id/:id/name/:name?',
+      pub: '/id/1/name/foo',
+      params: {
+        id: '1',
+        name: 'foo'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/id/:id/name/:name?',
+      pub: '/id/1/name/',
+      params: {
+        id: '1',
+        name: undefined
+      },
+      message: 'test'
+    },
+    {
+      sub: '/id/:id/name/:name?',
+      pub: '/id/1/name',
+      params: {
+        id: '1',
+        name: undefined
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name',
+      pub: '/1/foo',
+      params: {
+        id: '1',
+        name: 'foo'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?',
+      pub: '/1/foo',
+      params: {
+        id: '1',
+        name: 'foo'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?',
+      pub: '/1/',
+      params: {
+        id: '1',
+        name: undefined
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?',
+      pub: '/1',
+      params: {
+        id: '1',
+        name: undefined
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?/:age',
+      pub: '/1/foo/25',
+      params: {
+        id: '1',
+        name: 'foo',
+        age: '25'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?/aaa/bbb/ccc',
+      pub: '/1/foo/aaa/bbb/ccc',
+      params: {
+        id: '1',
+        name: 'foo'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id/:name?/aaa/bbb/:age?',
+      pub: '/1/foo/aaa/bbb/25',
+      params: {
+        id: '1',
+        name: 'foo',
+        age: '25'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id?/:name',
+      pub: '/1/foo',
+      params: {
+        id: '1',
+        name: 'foo'
+      },
+      message: 'test'
+    },
+    {
+      sub: '/:id?/:name?/:age?',
+      pub: '/1/foo/25',
+      params: {
+        id: '1',
+        name: 'foo',
+        age: '25'
+      },
+      message: 'test'
+    },
+  ];
 
+  let count = 0;
+  testcase.forEach((test) => {
+    let router = new Router();
     router.topic(test.sub, (message) => {
       deepEqual(message.params, test.params);
       assert(message.topic, test.pub);
       assert(message.body, test.message);
+      count++;
     });
 
     router.route(test.pub, test.message);
   });
+
+  assert(testcase.length, count);
 };
 
+function unmatch() {
+  let testcase = [
+    {
+      sub: '/id/:id/name/:name',
+      pub: '/id/1/name/'
+    },
+    {
+      sub: '/id/:id/name/:name',
+      pub: '/id/1/name'
+    },
+  ];
+
+  testcase.forEach((test) => {
+    let router = new Router();
+    router.topic(test.sub, (message) => {
+      throw new Error('cant be here');
+    });
+
+    router.route(test.pub, test.message);
+  });
+}
+
 function run() {
-  test1();
-  test2();
+  sample();
+  match();
+  unmatch();
 };
 run();
